ee12798674554e561424c3925def7484dd41c879
TomMarazzo <camenm@hotmail.com>
2023-12-16 14:52:34 -0500

    View formatting Updates

--- MGM/Models/CustomerOrder.cs
+++ MGM/Models/CustomerOrder.cs
@@ -8,10 +8,11 @@ namespace MGM.Models
         public Guid CustomerOrderId { get; set; }
         public virtual List<Customer>? Customers { get; set; } //List of Customers
         public int Qty { get; set; }
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
         public float Total { get; set; }
         public string Comments { get; set; }
-
+        [Display(Name = "Order Status")]
         public int OrderStatus { get; set; } //Days from Harvest
 
         public virtual List<CustomerOrderDetail>? CustomerOrderDetails { get; set; }
--- MGM/Models/Shelving.cs
+++ MGM/Models/Shelving.cs
@@ -8,6 +8,7 @@ namespace MGM.Models
         public Guid ShelvingId { get; set; }
         public int Row { get; set; }
         public int Column { get; set; }
+        [Display(Name = "Total Grow Spaces")]
         public int TotalGrowSpaces { get; set; }
         public virtual List<Supplier>? Suppliers { get; set; } //List of Suppliers
     }
