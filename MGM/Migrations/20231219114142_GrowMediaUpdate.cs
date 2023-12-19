dcd143f95ebf838ab7cdcc59046e69c0977ff2b5
TomMarazzo <camenm@hotmail.com>
2023-12-19 06:55:03 -0500

    Menu fixes but strill needs work.  Modified GrowMedia tables and Views

---
+++ MGM/Migrations/20231219114142_GrowMediaUpdate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MGM.Migrations
+{
+    /// <inheritdoc />
+    public partial class GrowMediaUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_costQties_GrowMedias_GrowMediaId",
+                table: "costQties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_costQties_GrowMediaId",
+                table: "costQties");
+
+            migrationBuilder.DropColumn(
+                name: "GrowMediaId",
+                table: "costQties");
+
+            migrationBuilder.AddColumn<float>(
+                name: "CurrentTotal",
+                table: "GrowMedias",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NoOfBags",
+                table: "GrowMedias",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "GrowMedias",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Qty",
+                table: "GrowMedias",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "RemainingTotal",
+                table: "GrowMedias",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Subtotal",
+                table: "GrowMedias",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Volume",
+                table: "GrowMedias",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentTotal",
+                table: "GrowMedias");
+
+            migrationBuilder.DropColumn(
+                name: "NoOfBags",
+                table: "GrowMedias");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "GrowMedias");
+
+            migrationBuilder.DropColumn(
+                name: "Qty",
+                table: "GrowMedias");
+
+            migrationBuilder.DropColumn(
+                name: "RemainingTotal",
+                table: "GrowMedias");
+
+            migrationBuilder.DropColumn(
+                name: "Subtotal",
+                table: "GrowMedias");
+
+            migrationBuilder.DropColumn(
+                name: "Volume",
+                table: "GrowMedias");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GrowMediaId",
+                table: "costQties",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_costQties_GrowMediaId",
+                table: "costQties",
+                column: "GrowMediaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_costQties_GrowMedias_GrowMediaId",
+                table: "costQties",
+                column: "GrowMediaId",
+                principalTable: "GrowMedias",
+                principalColumn: "GrowMediaId");
+        }
+    }
+}
--- MGM/Models/GrowMedia.cs
+++ MGM/Models/GrowMedia.cs
@@ -7,13 +7,25 @@ namespace MGM.Models
         [Key]
         public Guid GrowMediaId { get; set; }
         public string Type { get; set; }
-        //public float Qty { get; set; }
-        //[DisplayFormat(DataFormatString = "{0:c}")]
-        //[Range(0.01, 999999)]
-        //public double Price { get; set; }
+        [Display(Name = "No Of Bags")]
+        public int NoOfBags { get; set; }
+        public float Volume { get; set; }
+        public float Qty { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [Range(0.01, 999999)]
+        public double Price { get; set; }
         public string Description { get; set; }
+
+        public float Subtotal { get; set; }
+        [Display(Name = "Current Total")]
+        public float CurrentTotal { get; set; }
+        [Display(Name = "Remaining Total")]
+        public float RemainingTotal { get; set; }
         public virtual List<Supplier>? Suppliers { get; set; } //List of Suppliers
-        public virtual List<CostQty>? CostQties { get; set; } //List of CostQty
+
+
+        //public virtual List<CostQty>? CostQties { get; set; } //List of CostQty
 
     }
 }
